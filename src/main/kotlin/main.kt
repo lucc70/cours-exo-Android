0e85d2892659b53aff57b710901f3b66205778ea
lucc70 <luc.coujonde@gmail.com>
2021-05-22 01:46:35 +0200

    Partie 5 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,19 @@
 fun main(args: Array<String>) {
-    var aMultiple = 3
+    /*var aMultiple = 3
     print(multiFive(number = aMultiple))
 
     // autre manière plus courte
-    print(multiFive(aMultiple))
+    print(multiFive(aMultiple))*/
+
+
+    fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+    )
 }
 
-fun multiFive(number: Int): Int = 5 * number
-//⌥⏎
+// i lfaut rajouter des valeur par defaut au cas ou on decide de ne pas en entrer plus tard
+fun foo(name: String, number: Int = 0, toUpperCase: Boolean = true) =
+    (if (toUpperCase) name.toUpperCase() else name) + number
