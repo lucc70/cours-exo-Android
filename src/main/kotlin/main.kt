97010b940926361c31762a8ddab99ebdfa1786c2
lucc70 <luc.coujonde@gmail.com>
2021-05-22 02:07:17 +0200

    Partie6 bases clean

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,4 @@
 fun main(args: Array<String>) {
-    /*var aMultiple = 3
-    print(multiFive(number = aMultiple))
-
-    // autre manière plus courte
-    print(multiFive(aMultiple))*/
-
-    var num = 3
-    print("le factoriel de " + num + " est : " + factorielle(num))
+ 
 }
 
-//Implémenter l'algorithme permettant de calculer la factorielle d'un entier.
-fun factorielle (n : Int) : Int {
-    if (n>1) {
-        return n * factorielle(n-1)
-    } else {
-        return 1
-    }
-    return 1
-}
