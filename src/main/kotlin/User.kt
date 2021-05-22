0310256bc38ce00b765b779e5e3808fbc0cb51f4
lucc70 <luc.coujonde@gmail.com>
2021-05-22 02:34:12 +0200

    Partie 6 2+3+4

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,4 +1,12 @@
 
 //Créez une classe User qui contient deux attributs : nom et prénom
 open class User(private var nom: String?, private var prenom: String?) {
+
+    open fun display() {
+        println(nom + " " + prenom)
+    }
+    // on overrive la fun esquals qui renvoit
+    override fun equals(other: Any?): Boolean {
+        return (other as User).nom == nom && (other as User).prenom == prenom
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
-    
+
+    // utilisation de la fun equals pour comparer 2 users
+    println("sont ils égaux ? " + User("smith","toto").equals(User("smith","toto")))
+    User("titi","tata").display()
 }
 
