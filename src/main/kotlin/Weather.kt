4c9b0bd376bde3119895a05c19d2fa70e715d5ac
lucc70 <luc.coujonde@gmail.com>
2021-05-23 14:44:28 +0200

    Parie 6 4.4

--- src/main/kotlin/Weather.kt
+++ src/main/kotlin/Weather.kt
@@ -3,6 +3,7 @@ class Weather(var temperature: Float?, var lieu : String?) {
     // constructeur qui prend en obj Weather et copie les champs
     constructor(weather : Weather) :this(weather.temperature , weather.lieu)
     val seuilZero = 0;
+    val seuilChaud = 25f;
 
     fun display() {
         println(lieu + " " + temperature)
@@ -24,6 +25,16 @@ class Weather(var temperature: Float?, var lieu : String?) {
             return "Il fait plus de Zero degrè, donc non"
         }
         return "il fait Zero degrè, Donc je sais pas...";
+    }
+
+
+    fun isHot (): String {
+        if (this.temperature!! < seuilChaud) {
+            return "Il fait moins de 25 degrès, donc non pas trop chaud";
+        } else if (this.temperature!! == seuilChaud) {
+            return "Il fait 25 degrès, donc Oui"
+        }
+        return "Il fait plus de 25 degrès, donc Oui très";
     }
 
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,9 +11,9 @@ fun main(args: Array<String>) {
 
     //Weather(-5f, " Lyon")
 
-    var weather1 = Weather(temperature = 10f,lieu = "paris")
-    println("fait-il froid ? : \n - " + weather1.isCold())
-
+    var weather1 = Weather(temperature = 27f,lieu = "paris")
+    //println("fait-il froid ? : \n - " + weather1.isCold())
+    println("fait-il Chaud ? : \n - " + weather1.isHot())
 
 }
 
