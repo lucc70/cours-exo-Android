a94ac7d778a8d71dc3e21e38388b0ba41fa4cc2c
lucc70 <luc.coujonde@gmail.com>
2021-06-08 16:36:02 +0200

    ajout d'une petite demo du prof entre le 6 et le 7

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,11 +11,25 @@ fun main(args: Array<String>) {
 
     //Weather(-5f, " Lyon")
 
-    var weather1 = Weather(temperature = 27f,lieu = "paris")
+    //var weather1 = Weather(temperature = 27f,lieu = "paris")
     //println("fait-il froid ? : \n - " + weather1.isCold())
-    println("fait-il Chaud ? : \n - " + weather1.isHot())
-    var user1 = User("DOE","Smith")
-    user1.hello()
+    //println("fait-il Chaud ? : \n - " + weather1.isHot())
+    //var user1 = User("DOE","Smith")
+    //user1.hello()
+
+    /*var immutableList = listOf<Int>(1,2,3,4) //immutable
+    var mutableList = mutableListOf(1,2,3,4) //mutable
+
+    mutableList.add(3)
+    mutableList.remove(2)
+
+    arrayOf(1,2,3,4)
+
+    println(listOf(1,2,3,1))
+    println(setOf(1,2,3,1)) //impossible de faire 2 fois le meme index
+
+    val map = mapOf("clé" to "valeur")*/
+    //println(map[])
 
 
 }
